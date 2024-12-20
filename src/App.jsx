9bd8f185b8b9bbb27c40181e3543b8c6ca9bd65d
Valerio Casadei <valeriocasadei18@gmail.com>
2024-12-20 11:03:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<div className="max-w-screen-2xl flex flex-col justify-center md:min-h-screen"></div>
+			<div className="max-w-screen-xl flex flex-col justify-center md:min-h-screen"></div>
 		</>
 	);
 }
